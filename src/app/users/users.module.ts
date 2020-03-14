37598a82a52a9efc59668b3f04bede2618f049d2
valeryi <valeryi.oleinyk@gmail.com>
2020-03-14 14:54:09 +0200

    Cleaned the code

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { MaterialModule } from '../_helpers/material/material.module';
-import { FlexibleTableComponent } from '../_helpers/flexible-table/flexible-table.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     UsersComponent,
-    FlexibleTableComponent
   ],
   imports: [
     CommonModule,
